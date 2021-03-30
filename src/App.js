13c458471fe54e9b713ba01393d3992665586910
BenNiks1 <ikke.trodde@gmail.com>
2021-03-30 10:45:40 +0300

    font

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { HashRouter, Switch, Route } from "react-router-dom";
 import { Info } from "./Pages/Info";
 import { Payment } from "./Pages/Payment";
 import { Result } from "./Pages/Result";
@@ -7,13 +7,13 @@ import { Result } from "./Pages/Result";
 export const App = () => {
   return (
     <React.Fragment>
-      <BrowserRouter>
+      <HashRouter basename="/">
         <Switch>
           <Route exact path="/" component={Info} />
           <Route path="/payment" component={Payment} />
           <Route path="/result" component={Result} />
         </Switch>
-      </BrowserRouter>
+      </HashRouter>
     </React.Fragment>
   );
 };
